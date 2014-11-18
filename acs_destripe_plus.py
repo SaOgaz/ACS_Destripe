b14c4bf8e574c0d5a88d52073f08c8fbe896d98b
SaraOgaz <ogaz@stsci.edu>
2014-11-18 13:54:20 -0500

    changed arguments

--- acs_destripe_plus.py
+++ acs_destripe_plus.py
@@ -25,6 +25,7 @@ HISTORY:
          11SEP2014 Ogaz added capabilities for full frame processing
                    and stripe masking
          29OCT2014 Ogaz clean up for posting final script for users
+         18NOV2014 Ogaz changed options/switches
 
 
 '''
@@ -41,23 +42,23 @@ from acstools import acs2d
 from acstools import acscte
  
 
-def destripe_plus(sy,scimask1='None',scimask2='None',de_stripe=True):
+def destripe_plus(sy,scimask1='None',scimask2='None',cte_correct=True):
     # verify that the RAW image exists in cwd
     cwddir = os.getcwd()
     if not os.path.exists(cwddir+'/'+sy):
         sys.exit(sy + " RAW file does not exist. Quitting now!")
 
     #check if 2K subarray or full frame
-    cte_correct = True
     is_sub2K = False
-    ctecorr = fits.getval(sy,'PCTECORR')
-    aperture = fits.getval(sy,'APERTURE')
+    header = fits.getheader(sy)
+    ctecorr = header['PCTECORR']
+    aperture = header['APERTURE']
     subarray_list=['WFC1-2K', 'WFC1-POL0UV', 'WFC1-POL0V', 'WFC1-POL60V',
 					'WFC1-POL60UV', 'WFC1-POL120V', 'WFC1-POL120UV', 'WFC1-SMFL',
 					'WFC1-IRAMPQ', 'WFC1-MRAMPQ', 'WFC2-2K', 'WFC2-ORAMPQ',
 					'WFC2-SMFL', 'WFC2-POL0UV', 'WFC2-POL0V', 'WFC2-MRAMPQ']
 
-    if fits.getval(sy,'SUBARRAY') == True:
+    if header['SUBARRAY'] and cte_correct:
         if aperture in subarray_list:
             is_sub2K = True
         else:
@@ -68,22 +69,15 @@ def destripe_plus(sy,scimask1='None',scimask2='None',de_stripe=True):
     acsccd.acsccd(sy)
 
     # execute destriping of the subarray (post-SM4 data only)
-    if de_stripe == True:
-        acs_destripe.clean(sy.replace('raw','blv_tmp'), 'strp', clobber=False, maxiter=15, sigrej=2.0, mask1=scimask1, mask2=scimask2)
-        #TAKE THESE OUT!
-        #shutil.copyfile(sy.replace('raw','blv_tmp'),sy.replace('raw','blv_tmp_in'))
-        #shutil.copyfile(sy.replace('raw','blv_tmp_strp'),sy.replace('raw','blv_tmp_out'))
-        os.rename(sy.replace('raw','blv_tmp_strp'),sy.replace('raw','blv_tmp'))
+    acs_destripe.clean(sy.replace('raw','blv_tmp'), 'strp', clobber=False, maxiter=15, sigrej=2.0, mask1=scimask1, mask2=scimask2)
+    os.rename(sy.replace('raw','blv_tmp_strp'),sy.replace('raw','blv_tmp'))
 
     #update subarray header
-    if is_sub2K == True and cte_correct == True:
-        subarray = fits.open(sy.replace('raw','blv_tmp'), mode='update') # subarray
-        subhdr = subarray[0].header 
-        subhdr['pctecorr'] = 'PERFORM'
-        subarray.close()
+    if is_sub2K and cte_correct:
+        fits.setval(sy.replace('raw','blv_tmp'),'PCTECORR',value='PERFORM')
 
     # perform CTE correction on destriped image
-    if cte_correct == True:
+    if cte_correct:
     	if ctecorr == 'PERFORM':
         	acscte.acscte(sy.replace('raw','blv_tmp'))
         else:
@@ -92,18 +86,18 @@ def destripe_plus(sy,scimask1='None',scimask2='None',de_stripe=True):
         	
 	# run ACS2D to get FLT and FLC images
     acs2d.acs2d(sy.replace('raw','blv_tmp'))
-    if cte_correct == True:
+    if cte_correct:
         acs2d.acs2d(sy.replace('raw','blc_tmp'))
 
     # delete intermediate files
     os.system("rm "+sy.replace('raw','blv_tmp'))
-    if cte_correct == True:
+    if cte_correct:
         os.system("rm "+sy.replace('raw','blc_tmp')) 
  
     print '__________________________________________________________'
     print " "
     print "FLT : ", sy.replace('raw','flt')
-    if cte_correct == True:
+    if cte_correct:
         print "FLC : ", sy.replace('raw','flc')
     print " "
 
@@ -117,15 +111,15 @@ if __name__=='__main__':
                          Default is None if not using mask.')
     parser.add_argument('--sci2_mask',default='None', type=str, help='Input filename of mask for SCI 2. \
                          Default is None if not using mask.')
-    parser.add_argument('-d', '--destripe',default=True, type=bool, help='Use boolean values to turn de-striping \
+    parser.add_argument('-c', '--no_ctecorr',default=True, action='store_false', help='Turn cte correction off. \
                          on or off. Default to on (True).')
 
     options = parser.parse_args()
     if options.filename == 'NONE': 
         print "  "
-        print "Usage       :   python acs_destripe_plus.py -filename full-raw-filename -sci1_mask mask1.fits -sci2_mask mask2.fits"
-        print "For example :   python acs_destripe_plus.py -filename jc5001soq_raw.fits"
+        print "Usage       :   python acs_destripe_plus.py --filename full-raw-filename --sci1_mask mask1.fits --sci2_mask mask2.fits"
+        print "For example :   python acs_destripe_plus.py --filename jc5001soq_raw.fits"
         print " "        
         sys.exit()
 
-    destripe_plus(options.filename,scimask1=options.sci1_mask,scimask2=options.sci2_mask,de_stripe=options.destripe)
+    destripe_plus(options.filename,scimask1=options.sci1_mask,scimask2=options.sci2_mask,cte_correct=options.no_ctecorr)
